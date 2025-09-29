d159ec95b59ae263e55294b2c3af7da9147633e6
Maksim <maksimprucakov21@gmail.com>
2025-09-29 23:39:28 +0300

    Добавил вывод Привет мир

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace Тест_с_Git
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Привет мир");
         }
     }
 }
